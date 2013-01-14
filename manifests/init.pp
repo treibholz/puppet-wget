e6b7e6881b26fe9d20ed807bf731cea309453cc4
Dan Carley <dan.carley@gmail.com>
2013-01-14 10:39:33 +0000

    New version param for package ensure
    Allow a specific version to be passed into the Package[] resource. This
    could also be used to remove the package. However it will continue to have
    no effect on OSX platforms.
    
    The value `installed` is now quoted, in reality this makes little
    difference.

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,10 +4,10 @@
 # This class will install wget - a tool used to download content from the web.
 #
 ################################################################################
-class wget {
+class wget($version='installed') {
   
   if $::operatingsystem != 'Darwin' {
-    package { "wget": ensure => installed }
+    package { "wget": ensure => $version }
   }
 }
 
