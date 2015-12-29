07252b404356fd976d535f0f04b19d18b3efd78e
Klaus Umbach <klaus-github@uxix.de>
2015-12-29 16:52:05 +0100

    typo

--- manifests/fetch.pp
+++ manifests/fetch.pp
@@ -53,7 +53,7 @@ define wget::fetch (
     undef   => [],
     default => [ "HTTP_PROXY=${http_proxy}", "http_proxy=${http_proxy}" ],
   }
-  $https_proxy_env = $::https_proxy ? {
+  $https_proxy_env = $https_proxy ? {
     undef   => [],
     default => [ "HTTPS_PROXY=${https_proxy}", "https_proxy=${https_proxy}" ],
   }
